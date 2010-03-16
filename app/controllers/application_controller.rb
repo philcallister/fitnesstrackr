9928ebcac3e49762d6a86b9247ee7c490d6d3a2d
Phil Callister <phil@catch-23.(none)>
2010-03-15 21:51:41 -0500

    Form: Style

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,7 +32,7 @@ class ApplicationController < ActionController::Base
     def require_user
       unless current_user
         store_location
-        flash[:notice] = "You must be logged in to access this page"
+        flash[:notice] = "You must be signed in to access this page"
         redirect_to new_user_session_url
         return false
       end
@@ -41,7 +41,7 @@ class ApplicationController < ActionController::Base
     def require_no_user
       if current_user
         store_location
-        flash[:notice] = "You must be logged out to access this page"
+        flash[:notice] = "You must be signed out to access this page"
         redirect_to account_url
         return false
       end
